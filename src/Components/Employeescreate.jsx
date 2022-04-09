d4a55bff6f195107f1abfa674bc8a94675892df6
jaydeep2311 <100584963+jaydeep2311@users.noreply.github.com>
2022-04-09 17:27:15 +0530

    changes

---
+++ src/Components/Employeescreate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Employeescreate = () => {
+  return (
+    <div>
+      <input type="text" placeholder="name" name="name" />
+      <input type="text" name="age" placeholder="age" />
+      <input type="text" name="gender" placeholder="gender" />
+      <input type="text" placeholder="salary" name="salary" />
+      <input type="text" placeholder="department" name="dept" />
+    </div>
+  );
+};
+
+export default Employeescreate;
